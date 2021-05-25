f861b04512af50b0f7aa13790d27005c33303e4d
Felix Lemke <flemke.dev@gmail.com>
2021-05-25 22:19:36 +0200

    feat(algorithms): implement shell sort

--- src/algorithms-robert-sedgewick/2.1-shell-sort.ts
+++ src/algorithms-robert-sedgewick/2.1-shell-sort.ts
@@ -1,5 +1,22 @@
+
 type CompareFunction<T> = (a: T, b: T) => number;
 
 export function shellSort<T>(array: T[], compareFn: CompareFunction<T>): T[] {
-  return [];
+  const n = array.length;
+  let h = 1;
+
+  while (h < n / 3) {
+    h = 3 * h + 1;
+  }
+
+  while (h >= 1) {
+    for (let i = h; i < n; i++) {
+      for (let j = i; j >= h && compareFn(array[j - h], array[j]) < 0; j = j - h) {
+        [array[j], array[j - h]] = [array[j - h], array[j]];
+      }
+    }
+    h = Math.floor(h / 3);
+  }
+
+  return array;
 }
