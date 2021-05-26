efa7f4ebff6a22d33fd54a6da38d75f87772f9a5
Felix Lemke <flemke.dev@gmail.com>
2021-05-26 22:28:49 +0200

    feat(cracking): ants on a triangle

---
+++ src/cracking-the-coding-interview/6.4-ants-on-a-triangle.ts
@@ -0,0 +1,7 @@
+/**
+ * There are three ants on different vertices of a triangle. What is the probability
+ * of collision (between any two or all of them) if they start walking on the sides
+ * of the triangle? Assume that each ant randomly picks a direction, with either
+ * direction being equally likely to be chosen, and that they walk at the same speed.
+ */
+export const probability = 3 * (0.5 * 0.5);
---
+++ src/cracking-the-coding-interview/tests/6.4-ants-on-a-triangle.spec.ts
@@ -0,0 +1,7 @@
+import { probability } from '../6.4-ants-on-a-triangle';
+
+describe('Ants on a Triangle', () => {
+  it('should have the probability of 0.75', () => {
+    expect(probability).toBe(0.75);
+  });
+})
